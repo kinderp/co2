3109c08c10f2d667e0511c02296ae7cdc09e02fb
Antonio <a.caristia@gmail.com>
2020-10-10 12:02:44 +0200

    Add variable types annotations in TNodesBitmap

--- co2/core/fs/t_nodes_bitmap.py
+++ co2/core/fs/t_nodes_bitmap.py
@@ -7,11 +7,11 @@ class TNodesBitmap:
         cls.bitmap[t_node_number] = True
 
     @classmethod
-    def _rem(cls, t_node_number : int):
+    def _rem(cls, t_node_number : int) -> int:
         return cls.bitmap.pop(t_node_number, None)
 
     @classmethod
-    def _get(cls):
+    def _get(cls) -> int:
         for t_node_number in range(0, max(cls.bitmap.keys())+1):
             if not cls.bitmap.get(t_node_number, None):
                 return t_node_number
