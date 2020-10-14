4e49007c103613fcf7758808aeb11ec71765c439
Antonio <a.caristia@gmail.com>
2020-10-14 09:02:06 +0200

    Add fondamental I/O  system calls
    (*) mkdir  : create a  directory
    (*) rmdir  : delete a  directory
    (*) open   : open file/directory
    (*) unlink : delete a file
    
    Example:
    
    1. Create a new "root"  dir under /
    2. Create a new "file1" file under /root
    3. Open   "/root/file1"
    4. Remove "/root/file1"
    5. Remove "/root"
    
    Corresponding system calls:
    
    - do_mkdir("/root", OFlags.O_CREAT | OFlags.O_WRONLY)
    - do_open("/root/file1", OFlags.O_CREAT | OFlags.O_WRONLY)
    - do_open("/root/file1", OFlags.O_RDONLY)
    - do_unlink("/root/file1")
    - do_rmdir("/root")
    
    	from co2.core.fs import Fs
    	from co2.core.fs.fs import OFlags
    	fs = Fs()
    	fs.superblock.vector.get_entry(0).block.render() # very low level ls
    	fs.do_mkdir("/root",      OFlags.O_CREAT | OFlags.O_WRONLY)
    	fs.do_open("/root/file1", OFlags.O_CREAT | OFlags.O_WRONLY)
    	fs.superblock.vector.get_entry(0).block.render()
    	fs.do_unlink("/root/file1")
    	fs.superblock.vector.get_entry(0).block.render()
    	fs.do_rmdir("/root")
    	fs.superblock.vector.get_entry(0).block.render()

--- co2/core/fs/fs.py
+++ co2/core/fs/fs.py
@@ -12,9 +12,146 @@ class OFlags:
     # Bits OR'd in the 2nd argument of Open
     O_CREAT = 0x00200 # 512
 
+
+class TraverseCases:
+        CASE_1 = 0
+        CASE_2 = 1
+        CASE_3 = 2
+
+class HandlersTypes:
+        ADD_A_NODE  = 0
+        DEL_A_NODE  = 1
+        OPEN_A_NODE = 2
+
+
+class HandlersFunctions:
+        @staticmethod
+        def add_a_node(superblock : Superblock, case : int, filename : str, t_node : TNode, type : Types):
+            if case == TraverseCases.CASE_1:
+                return False
+            elif case == TraverseCases.CASE_2:
+                free_t_node_number = superblock.reserve_t_node_number()
+                superblock.vector.add_entry(
+                    vector_entry_index = free_t_node_number,
+                    vector_entry       = TNode(filename=filename,
+                                               block=Block(name=filename,
+                                                           parent=t_node.block,),
+                                               type = type
+                                         )
+                )
+                t_node.add_dir_entry(free_t_node_number, filename)
+                return True
+            elif case == TraverseCases.CASE_3:
+                return False
+            else:
+                # Not supported cases
+                return False
+
+        @staticmethod
+        def del_a_ndoe(superblock : Superblock, case : int, filename : str, t_node : TNode,
+                       t_node_number : int):
+            if case == TraverseCases.CASE_1:
+                superblock.release_t_node_number(t_node_number)
+                superblock.vector.rem_entry(t_node_number)
+                old_children = list(t_node.block.children) # children it's a tuple :(
+                for index, children in enumerate(old_children):
+                    if children.name == filename:
+                        old_children.pop(index)
+                t_node.block.children = old_children
+                return True
+            elif case == TraverseCases.CASE_2:
+                return False
+            elif case == TraverseCases.CASE_3:
+                return False
+            else:
+                # Not supported cases
+                return False
+
+        @staticmethod
+        def open_a_node(superblock : Superblock, case : int):
+            if case == TraverseCases.CASE_1:
+                return True
+            elif case == TraverseCases.CASE_2:
+                return False
+            elif case == TraverseCases.CASE_3:
+                return False
+            else:
+                # Not supported cases
+                return False
+
+
 class Fs:
 
     superblock = Superblock()
+
+    handlers = {
+        HandlersTypes.ADD_A_NODE  : HandlersFunctions.add_a_node,
+        HandlersTypes.DEL_A_NODE  : HandlersFunctions.del_a_ndoe,
+        HandlersTypes.OPEN_A_NODE : HandlersFunctions.open_a_node,
+    }
+
+    @classmethod
+    def _traverse_path2(cls, path_tokens            : list,
+                            level                   : int,
+                            t_node_number           : int,
+                            handler_type            : int,
+                            handler_function_kwargs : dict,
+                      ):
+        current_branch = path_tokens[level]
+        t_node = cls.superblock.vector.get_entry(t_node_number)
+        next_t_node_number = t_node.dir_table._get(current_branch)
+        if next_t_node_number and (level + 1 ) == len(path_tokens):
+            # Case#1: Found a leaf
+            #
+            # ADD_A_NODE  : File already exist  KO
+            # OPEN_A_NODE : File found          OK
+            # DEL_A_NODE  : File found          OK
+            if handler_type == HandlersTypes.ADD_A_NODE:
+                return cls.handlers[handler_type](cls.superblock,
+                                                  TraverseCases.CASE_1, **handler_function_kwargs)
+            elif handler_type == HandlersTypes.OPEN_A_NODE:
+                return cls.handlers[handler_type](cls.superblock,
+                                                  TraverseCases.CASE_1, **handler_function_kwargs)
+                pass
+            elif handler_type == HandlersTypes.DEL_A_NODE:
+                handler_function_kwargs.update({"filename"      : current_branch}     )
+                handler_function_kwargs.update({"t_node"        : t_node}             )
+                handler_function_kwargs.update({"t_node_number" : next_t_node_number} )
+                return cls.handlers[handler_type](cls.superblock,
+                                                  TraverseCases.CASE_1, **handler_function_kwargs)
+            else:
+                pass
+        elif not next_t_node_number and (level + 1) == len(path_tokens):
+            # Case#2: Not found a leaf
+            #
+            # ADD_A_NODE  : does not exist a file with the same name OK
+            # OPEN_A_NODE : file does not exist                      KO
+            # DEL_A_NODE  : file does not exist                      KO
+            if handler_type == HandlersTypes.ADD_A_NODE:
+                handler_function_kwargs.update({"filename": current_branch} )
+                handler_function_kwargs.update({"t_node"  : t_node}         )
+                return cls.handlers[handler_type](cls.superblock,
+                                                  TraverseCases.CASE_2, **handler_function_kwargs)
+            elif handler_type == HandlersTypes.OPEN_A_NODE:
+                return cls.handlers[handler_type](cls.superblock,
+                                                  TraverseCases.CASE_2, **handler_function_kwargs)
+            elif handler_type == HandlersTypes.DEL_A_NODE:
+                return cls.handlers[handler_type](cls.superblock,
+                                                  TraverseCases.CASE_2, **handler_function_kwargs)
+            else:
+                pass
+        elif not next_t_node_number and (level + 1) < len(path_tokens):
+            # Case#3: Not found path
+            #
+            # ADD_A_NODE  : Path does not exist     KO
+            # OPEN_A_NODE : Path does not exist     KO
+            # DEL_A_NODE  : Path does not exist     KO
+            return cls.handlers[handler_type](cls.superblock,
+                                              TraverseCases.CASE_3, **handler_function_kwargs)
+        else:
+            return cls._traverse_path2(path_tokens, level + 1,
+                                       next_t_node_number, handler_type,
+                                       handler_function_kwargs)
 
     @classmethod
     def _traverse_path(cls, path_tokens   : list,
@@ -53,12 +190,28 @@ class Fs:
     @classmethod
     def _new_node(cls, filename : str, type : Types):
         path_tokens = filename.split("/")[1:]
-        return cls._traverse_path(path_tokens, 0, 0, True, type)
+        #return cls._traverse_path(path_tokens, 0, 0, True, type)
+        return cls._traverse_path2(path_tokens, 0, 0,
+                                   HandlersTypes.ADD_A_NODE,
+                                   {
+                                        "filename": None,
+                                        "t_node"  : None,
+                                        "type"    : type
+                                   },
+                                  )
 
     @classmethod
     def _eat_path(cls, filename : str):
         path_tokens = filename.split("/")[1:]
-        return cls._traverse_path(path_tokens, 0, 0, False, None)
+        #return cls._traverse_path(path_tokens, 0, 0, False, None)
+        return cls._traverse_path2(path_tokens, 0, 0,
+                                   HandlersTypes.OPEN_A_NODE, {})
+
+    @classmethod
+    def _del_node(cls, filename : str):
+        path_tokens = filename.split("/")[1:]
+        return cls._traverse_path2(path_tokens, 0, 0,
+                                   HandlersTypes.DEL_A_NODE, {})
 
     @classmethod
     def do_open(cls, abs_filename : str,
@@ -70,9 +223,17 @@ class Fs:
         return cls._eat_path(abs_filename)
 
     @classmethod
-    def do_mkdir(cls, abs_filename):
+    def do_mkdir(cls, abs_filename, oflags : OFlags):
         cls.do_open(abs_filename, OFlags.O_WRONLY | OFlags.O_CREAT,
                     Types.DIRECTORY)
+
+    @classmethod
+    def do_rmdir(cls, abs_filename : str):
+        return cls._del_node(abs_filename)
+
+    @classmethod
+    def do_unlink(cls, abs_filename : str):
+        return cls._del_node(abs_filename)
 
     def __init__(self):
         pass
