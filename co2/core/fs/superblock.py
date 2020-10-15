58d3a0a73eae89be54948c42eb22581981a558f7
Antonio <a.caristia@gmail.com>
2020-10-15 13:34:36 +0200

    Switch superblock fields from instance to class property

--- co2/core/fs/superblock.py
+++ co2/core/fs/superblock.py
@@ -2,22 +2,16 @@ from .t_nodes_bitmap import TNodesBitmap
 from .t_nodes_vector import TNodesVector
 
 class Superblock:
-    def __init__(self):
-        self.__t_nodes_bitmap = TNodesBitmap()
-        self.__t_nodes_vector = TNodesVector()
 
-    @property
-    def bitmap(self):
-        return self.__t_nodes_bitmap
+    bitmap = TNodesBitmap()
+    vector = TNodesVector()
 
-    @property
-    def vector(self):
-        return self.__t_nodes_vector
-
-    def reserve_t_node_number(self) -> int :
-        reserved = self.__t_nodes_bitmap._get()
-        self.__t_nodes_bitmap._add(reserved)
+    @classmethod
+    def reserve_t_node_number(cls) -> int :
+        reserved = cls.bitmap._get()
+        cls.bitmap._add(reserved)
         return reserved
 
-    def release_t_node_number(self, t_node_number : int) -> int:
-        return self.__t_nodes_bitmap._rem(t_node_number)
+    @classmethod
+    def release_t_node_number(cls, t_node_number : int) -> int:
+        return cls.bitmap._rem(t_node_number)
