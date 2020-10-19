af7ee04692ea3698445ed5634c922da41851f3a9
Antonio <a.caristia@gmail.com>
2020-10-19 18:51:44 +0200

    Add call to MessageFactory in CommandTouch
    python3 main.py touch /file1

--- co2/commands/command_touch.py
+++ co2/commands/command_touch.py
@@ -1,11 +1,22 @@
 from co2.commands import Command
+from co2.commands import CO2Commands
+from co2.ipc import Client
+from co2.ipc.messages import MessagesFactory
 
 class CommandTouch(Command):
     def init(self, argsubparsers):
         argsp = argsubparsers.add_parser("touch", help="Create a new file")
+        argsp.add_argument("path", help="Create a file", type=str)
 
     def execute(self, args):
-        print("touch...")
+        client = Client()
+        client.execute(
+            MessagesFactory.create(
+                CO2Commands.CO2_TOUCH,
+                CO2Commands.look_up(CO2Commands.CO2_TOUCH),
+                args,
+            ).to_dict()
+        )
 
     def __init__(self, argsubparsers):
         self.init(argsubparsers)
