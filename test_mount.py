5ebd84cc78b772c256bc0290e17aff5499b09d16
Antonio <a.caristia@gmail.com>
2021-02-28 17:50:37 +0100

    Modify test_mount.py after FTable and Process fixes

--- test_mount.py
+++ test_mount.py
@@ -1,78 +1,86 @@
+from co2.system_calls import ProcessSystemCalls
 from co2.system_calls import IOSystemCalls
 from co2.core.fs.fs import OFlags
 
 import ipdb
 ipdb.set_trace()
 
-assert IOSystemCalls.open('/file', OFlags.O_CREAT | OFlags.O_WRONLY)
+ProcessSystemCalls.boot()
+assert IOSystemCalls.open('/file', OFlags.O_RDONLY) < 0 # file does not exist, error
+assert IOSystemCalls.open('/file', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/file', OFlags.O_RDONLY) > 0
 
-assert IOSystemCalls.mkdir('/dev')
-assert IOSystemCalls.mkdir('/etc')
-assert IOSystemCalls.open('/etc/resolv.conf', OFlags.O_CREAT | OFlags.O_WRONLY)
+# /dev is created in boot()
+#assert IOSystemCalls.mkdir('/dev') > 0
+assert IOSystemCalls.mkdir('/etc') > 0
+assert IOSystemCalls.open('/etc/resolv.conf', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
 
-assert IOSystemCalls.mkdir('/usr')
-assert IOSystemCalls.mkdir('/usr/local')
+assert IOSystemCalls.mkdir('/usr') > 0
+assert IOSystemCalls.mkdir('/usr/local') > 0
 
-assert IOSystemCalls.mkdir('/bin')
+# /bin is created in boot()
+#assert IOSystemCalls.mkdir('/bin') > 0
 
-assert IOSystemCalls.mkdir('/home')
-assert IOSystemCalls.mkdir('/home/user1')
-assert IOSystemCalls.open('/home/user1/.profile', OFlags.O_CREAT | OFlags.O_WRONLY)
+assert IOSystemCalls.mkdir('/home') > 0
+assert IOSystemCalls.mkdir('/home/user1') > 0
+assert IOSystemCalls.open('/home/user1/.profile', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
 
-assert IOSystemCalls.mkdir('/home/user2')
-assert IOSystemCalls.open('/home/user2/.profile', OFlags.O_CREAT | OFlags.O_WRONLY)
+assert IOSystemCalls.mkdir('/home/user2') > 0
+assert IOSystemCalls.open('/home/user2/.profile', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
 
-assert IOSystemCalls.mkdir('/home/user3')
-assert IOSystemCalls.open('/home/user3/.profile', OFlags.O_CREAT | OFlags.O_WRONLY)
+assert IOSystemCalls.mkdir('/home/user3') > 0
+assert IOSystemCalls.open('/home/user3/.profile', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
 
-assert IOSystemCalls.mknod('/dev/hda', 1, 1)
-assert IOSystemCalls.mknod('/dev/hdb', 1, 2)
-assert IOSystemCalls.mknod('/dev/hdc', 1, 3)
-assert IOSystemCalls.mknod('/dev/hdd', 1, 4)
+assert IOSystemCalls.mknod('/dev/hda', 1, 1) > 0
+assert IOSystemCalls.mknod('/dev/hdb', 1, 2) > 0
+assert IOSystemCalls.mknod('/dev/hdc', 1, 3) > 0
+assert IOSystemCalls.mknod('/dev/hdd', 1, 4) > 0
 
-assert IOSystemCalls.mkdir('/mnt')
-assert IOSystemCalls.mkdir('/mnt/hda')
-assert IOSystemCalls.mkdir('/mnt/hdb')
-assert IOSystemCalls.mkdir('/mnt/hdc')
-assert IOSystemCalls.mkdir('/mnt/hdd')
+assert IOSystemCalls.mkdir('/mnt') > 0
+assert IOSystemCalls.mkdir('/mnt/hda') > 0
+assert IOSystemCalls.mkdir('/mnt/hdb') > 0
+assert IOSystemCalls.mkdir('/mnt/hdc') > 0
+assert IOSystemCalls.mkdir('/mnt/hdd') > 0
 
-assert IOSystemCalls.mount('hda', '/mnt/hda')
-assert IOSystemCalls.mount('hdb', '/mnt/hdb')
-assert IOSystemCalls.mount('hdc', '/mnt/hdc')
-assert IOSystemCalls.mount('hdd', '/mnt/hdd')
+assert IOSystemCalls.mount('hda', '/mnt/hda') > 0
+assert IOSystemCalls.mount('hdb', '/mnt/hdb') > 0
+assert IOSystemCalls.mount('hdc', '/mnt/hdc') > 0
+assert IOSystemCalls.mount('hdd', '/mnt/hdd') > 0
 
-assert IOSystemCalls.mkdir('/mnt/hda/test')
-assert IOSystemCalls.open('/mnt/hda/test/1', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hda/test/2', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.mkdir('/mnt/hda/test2')
-assert IOSystemCalls.open('/mnt/hda/test2/1', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hda/test2/2', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hda/test2/3', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hda/test2/4', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.rmdir('/mnt/hda/test2')
+assert IOSystemCalls.mkdir('/mnt/hda/test') > 0
+assert IOSystemCalls.open('/mnt/hda/test/1', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hda/test/2', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.mkdir('/mnt/hda/test2') > 0
+assert IOSystemCalls.open('/mnt/hda/test2/1', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+import pdb
+pdb.set_trace()
+assert IOSystemCalls.open('/mnt/hda/test2/2', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hda/test2/3', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hda/test2/4', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.rmdir('/mnt/hda/test2') > 0
 
-assert IOSystemCalls.umount('hda', '/mnt/hda')
+assert IOSystemCalls.umount('hda', '/mnt/hda') > 0
 
-assert IOSystemCalls.mkdir('/mnt/hdb/test')
-assert IOSystemCalls.open('/mnt/hdb/test/1', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdb/test/2', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.mkdir('/mnt/hdb/test2')
-assert IOSystemCalls.open('/mnt/hdb/test2/1', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdb/test2/2', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdb/test2/3', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdb/test2/4', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.rmdir('/mnt/hdb/test2')
+assert IOSystemCalls.mkdir('/mnt/hdb/test') > 0
+assert IOSystemCalls.open('/mnt/hdb/test/1', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdb/test/2', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.mkdir('/mnt/hdb/test2') > 0
+assert IOSystemCalls.open('/mnt/hdb/test2/1', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdb/test2/2', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdb/test2/3', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdb/test2/4', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.rmdir('/mnt/hdb/test2') > 0
 
-assert IOSystemCalls.umount('hdb', '/mnt/hdb')
+assert IOSystemCalls.umount('hdb', '/mnt/hdb') > 0
 
-assert IOSystemCalls.mkdir('/mnt/hdc/test')
-assert IOSystemCalls.open('/mnt/hdc/test/1', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdc/test/2', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.mkdir('/mnt/hdc/test2')
-assert IOSystemCalls.open('/mnt/hdc/test2/1', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdc/test2/2', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdc/test2/3', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.open('/mnt/hdc/test2/4', OFlags.O_CREAT | OFlags.O_WRONLY)
-assert IOSystemCalls.rmdir('/mnt/hdc/test2')
+assert IOSystemCalls.mkdir('/mnt/hdc/test') > 0
+assert IOSystemCalls.open('/mnt/hdc/test/1', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdc/test/2', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.mkdir('/mnt/hdc/test2') > 0
+assert IOSystemCalls.open('/mnt/hdc/test2/1', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdc/test2/2', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdc/test2/3', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.open('/mnt/hdc/test2/4', OFlags.O_CREAT | OFlags.O_WRONLY) > 0
+assert IOSystemCalls.rmdir('/mnt/hdc/test2')> 0
 
-assert IOSystemCalls.umount('hdc', '/mnt/hdc')
+assert IOSystemCalls.umount('hdc', '/mnt/hdc') > 0
